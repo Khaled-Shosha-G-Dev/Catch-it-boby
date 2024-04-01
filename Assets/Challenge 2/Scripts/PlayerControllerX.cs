0909ac416a6b22824112976381070067d52dc463
Khaled Hossam Ahmed <khaledhossamG.dev@gmail.com>
2024-04-01 23:32:20 +0200

    initial commet

---
+++ Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    private bool isReadyToSpawn=true;
+    [SerializeField] private float Delay;
+    // Update is called once per frame
+    void Update()
+    {
+
+        // On spacebar press, send dog
+        if(Input.GetKeyDown(KeyCode.Space)&&isReadyToSpawn)
+            SpawnDogs();
+    }
+    void SpawnDogs()
+    {
+        Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+        isReadyToSpawn=false;
+        StartCoroutine(TimeSpawning());
+    }
+    IEnumerator TimeSpawning()
+    {
+        yield return new WaitForSeconds(Delay);
+        isReadyToSpawn=true;
+    }
+}
